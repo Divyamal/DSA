b2a2f361d62fe9ae54b0246a2ebf59ead2d28855
Divya <divyamohan.kp>
2024-12-27 13:00:01 +0530

    first commit

---
+++ src/BinarySearch.java
@@ -0,0 +1,30 @@
+public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] arr = {2,8,9,12,14,20,40};
+        int target = 12;
+        int ans = binarySearch(arr, target);
+        System.out.println(ans);
+    }
+
+    static int binarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end) {
+            // find the middle element
+//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+            int mid = start + (end - start) / 2;
+
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                // ans found
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
